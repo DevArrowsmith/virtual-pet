486cfd2e46ba945e38d9bcf10dff857ce71c348d
devarrowsmith <devarrowsmith@gmail.com>
2021-05-12 22:10:24 +0100

    Applied doublequotes to strings in pet.test.js.

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,10 +1,10 @@
 const Pet = require("../src/pet");
 
-describe('Pet', () => {
-    it('returns an object', () => {
+describe("Pet", () => {
+    it("returns an object", () => {
         expect(new Pet("Fido")).toBeInstanceOf(Object);
     });
-    it('returns an object with a specified name property', () => {
+    it("returns an object with a specified name property", () => {
         expect(new Pet("Pompompur").name).toBe("Pompompur");
     });
 });
