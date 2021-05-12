6495a5a1e942beae63dbf2920071514e7e4c94bc
devarrowsmith <devarrowsmith@gmail.com>
2021-05-12 22:35:44 +0100

    Create test: Pet: returns an object with an initial 'hunger' value of 0.

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -10,6 +10,9 @@ describe("Pet", () => {
     it("returns an object with an initial 'age' value of 0", () => {
         expect(new Pet("Sparky Buttons").age).toBe(0);
     })
+    it("returns an object with an initial 'hunger' value of 0", () => {
+        expect(new Pet("This Guy").hunger).toBe(0);
+    })
     it("has a growUp method that increases the 'age' value by 1", () => {
         const whiskeridoo = new Pet("Whiskeridoo");
         expect(whiskeridoo.age).toBe(0);
