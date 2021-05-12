34ebfc003f8e2319acb19810f40ff6fbc235aa9c
devarrowsmith <devarrowsmith@gmail.com>
2021-05-12 22:51:27 +0100

    Refactored tests using beforeAll, beforeEach and afterEach.

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,28 +1,40 @@
 const Pet = require("../src/pet");
 
 describe("Pet", () => {
+
+    beforeAll(() => {
+        let fido = {};
+    })
+
+    beforeEach(() => {
+        fido = new Pet("Fido");
+    });
+
+    afterEach(() => {
+        fido = {};
+    })
+
     it("returns an object", () => {
-        expect(new Pet("Fido")).toBeInstanceOf(Object);
+        expect(new Pet("Pompompur")).toBeInstanceOf(Object);
     });
     it("returns an object with a specified 'name' value", () => {
-        expect(new Pet("Pompompur").name).toBe("Pompompur");
+        expect(fido.name).toBe("Fido");
     });
     it("returns an object with an initial 'age' value of 0", () => {
-        expect(new Pet("Sparky Buttons").age).toBe(0);
+        expect(fido.age).toBe(0);
     });
     it("returns an object with an initial 'hunger' value of 0", () => {
-        expect(new Pet("This Guy").hunger).toBe(0);
+        expect(fido.hunger).toBe(0);
     });
     it("returns an object with an initial 'fitness' value of 10", () => {
-        expect(new Pet("Mr. Sinister").fitness).toBe(10);
+        expect(fido.fitness).toBe(10);
     });
     it("has a growUp method that increases the 'age' value by 1", () => {
-        const whiskeridoo = new Pet("Whiskeridoo");
-        expect(whiskeridoo.age).toBe(0);
-        whiskeridoo.growUp();
-        expect(whiskeridoo.age).toBe(1);
-        whiskeridoo.growUp();
-        whiskeridoo.growUp();
-        expect(whiskeridoo.age).toBe(3);
+        expect(fido.age).toBe(0);
+        fido.growUp();
+        expect(fido.age).toBe(1);
+        fido.growUp();
+        fido.growUp();
+        expect(fido.age).toBe(3);
     })
 });
