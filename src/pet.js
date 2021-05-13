ec25cc121a079307eecd6406d8126043cafd92f2
devarrowsmith <devarrowsmith@gmail.com>
2021-05-13 21:52:29 +0100

    Refactor pet.js, replacing magic numbers with named constants.

--- src/pet.js
+++ src/pet.js
@@ -1,31 +1,44 @@
+const INITIAL_AGE = 0;
+const INITIAL_HUNGER = 0
+const INITIAL_FITNESS = 10;
+
 const MAX_FITNESS = 10;
+const FITNESS_THRESHOLD = 3;
+
 const MIN_HUNGER = 0;
+const HUNGER_THRESHOLD = 5
+
+const FEED_HUNGER_DECREASE = 3;
+const EXERCISE_FITNESS_INCREASE = 4;
+
+const AGING_HUNGER_INCREASE = 5;
+const AGING_FITNESS_DECREASE = 3;
 
 function Pet (name) {
     this.name = name;
-    this.age = 0;
-    this.hunger = 0;
-    this.fitness = 10;
+    this.age = INITIAL_AGE;
+    this.hunger = INITIAL_HUNGER;
+    this.fitness = INITIAL_FITNESS;
 };
 
 Pet.prototype = {
     growUp () {
         this.age ++;
-        this.hunger += 5;
-        this.fitness -= 3;
+        this.hunger += AGING_HUNGER_INCREASE;
+        this.fitness -= AGING_FITNESS_DECREASE;
     },
     feed () {
-        this.hunger = Math.max(this.hunger - 3, MIN_HUNGER);
+        this.hunger = Math.max(this.hunger - FEED_HUNGER_DECREASE, MIN_HUNGER);
     },
     exercise () {
-        this.fitness = Math.min(this.fitness + 4, MAX_FITNESS);
+        this.fitness = Math.min(this.fitness + EXERCISE_FITNESS_INCREASE, MAX_FITNESS);
     },
     checkUp () {
-        if(this.fitness <= 3 && this.hunger >= 5) {
+        if(this.fitness <= FITNESS_THRESHOLD && this.hunger >= HUNGER_THRESHOLD) {
             return "I'm hungry and I need a walk!"
-        } else if (this.fitness <= 3) {
+        } else if (this.fitness <= FITNESS_THRESHOLD) {
             return "I need a walk!"
-        } else if (this.hunger >= 5) {
+        } else if (this.hunger >= HUNGER_THRESHOLD) {
             return "I'm hungry!"
         };
     }
