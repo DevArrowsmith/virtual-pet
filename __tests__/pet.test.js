bab0708763e3bb98154a6c017ebb255e4d25472f
devarrowsmith <devarrowsmith@gmail.com>
2021-05-17 20:01:04 +0100

    Refactor tests in preparation for guard clauses.

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -22,29 +22,22 @@ describe("Pet", () => {
         expect(fido.fitness).toBe(10);
     });
 
-    describe("growUp()", () => {
+    describe("Pet.growUp()", () => {
         it("increases the 'age' value by 1", () => {
+            console.log("Start of Age", fido.isAlive);
             expect(fido.age).toBe(0);
             fido.growUp();
             expect(fido.age).toBe(1);
-            fido.growUp();
-            fido.growUp();
-            expect(fido.age).toBe(3);
         });
         it("increases the 'hunger' value by 5", () => {
             expect(fido.hunger).toBe(0);
             fido.growUp();
             expect(fido.hunger).toBe(5);
-            fido.growUp();
-            expect(fido.hunger).toBe(10);
         });
         it("decreases the 'fitness' value by 3", () => {
             expect(fido.fitness).toBe(10);
             fido.growUp();
             expect(fido.fitness).toBe(7);
-            fido.growUp();
-            fido.growUp();
-            expect(fido.fitness).toBe(1);
         });
     });
 
@@ -85,19 +78,19 @@ describe("Pet", () => {
     });
 
     describe("Pet.checkUp()", () => {
-        it("returns the string 'I need a walk!' if fitness is 3 or lower.", () => {
+        it("returns the string 'I need a walk!' if fitness is 3 or lower", () => {
             fido.fitness = 3;
             expect(fido.checkUp()).toBe("I need a walk!");
             fido.fitness = 1;
             expect(fido.checkUp()).toBe("I need a walk!");
         });
-        it("returns the string 'I'm hungry!' if hunger is 5 or higher.", () => {
+        it("returns the string 'I'm hungry!' if hunger is 5 or higher", () => {
             fido.hunger = 5;
             expect(fido.checkUp()).toBe("I'm hungry!");
             fido.hunger = 9;
             expect(fido.checkUp()).toBe("I'm hungry!");
         });
-        it("returns the string 'I'm hungry and I need a walk!' if hunger is 5 or higher and fitness is 3 or lower.", () => {
+        it("returns the string 'I'm hungry and I need a walk!' if hunger is 5 or higher and fitness is 3 or lower", () => {
             fido.hunger = 5;
             fido.fitness = 3;
             expect(fido.checkUp()).toBe("I'm hungry and I need a walk!")
@@ -105,7 +98,7 @@ describe("Pet", () => {
             fido.fitness = 1;
             expect(fido.checkUp()).toBe("I'm hungry and I need a walk!");        
         });
-        it("returns the string 'I feel great!' if hunger is 4 or lower and fitness is 4 or higher.", () => {
+        it("returns the string 'I feel great!' if hunger is 4 or lower and fitness is 4 or higher", () => {
             expect(fido.checkUp()).toBe("I feel great!")
             fido.hunger = 4;
             fido.fitness = 4;
@@ -117,19 +110,19 @@ describe("Pet", () => {
         it("has an initial value of true.", () => {
             expect(fido.isAlive).toBe(true);
         });
-        it("returns false when hunger is 10 or above.", () => {
+        it("returns false when hunger is 10", () => {
             fido.hunger = 9;
             expect(fido.isAlive).toBe(true);
             fido.hunger = 10;
             expect(fido.isAlive).toBe(false);
         });
-        it("returns false when fitness is 0 or lower.", () => {
+        it("returns false when fitness is 0", () => {
             fido.fitness = 1;
             expect(fido.isAlive).toBe(true);
             fido.fitness = 0;
             expect(fido.isAlive).toBe(false);
         });
-        it("returns false when age is 30 or above.", () => {
+        it("returns false when age is 30", () => {
             fido.age = 29;
             expect(fido.isAlive).toBe(true);
             fido.age = 30;
